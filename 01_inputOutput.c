1799b44de9f4e26c1dd57dee6e98308696f30f19
rahulkumarclg <rahul.code2024@gmail.com>
2025-08-18 11:38:00 +0530

    c language of basics

---
+++ 01_inputOutput.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+int a;
+int b;
+//============== input and output
+// scanf("%d", &a);
+// printf("%d", a);
+
+// scanf("%2d %3d", &a, &b); //If you type the space input time than it will auto detect second number.
+// printf("%d %d", a, b);
+
+//printf = minimum
+//scanf = mazimum
+//==============input based condition 
+// scanf("%2d", &a); //Maximum 2 digit print
+// printf("%d", a);
+
+///============output based condition
+
+// scanf("%d", &a);
+// printf("%4d", a);//minimum 4 digit printing
+
+
+
+return 0;
+}
---
+++ 02_floatInput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+float a;
+float b;
+// if you give the input with space than it will auto detect second number.
+scanf("%f%f", &a, &b);
+printf("A value of a is: = %f, A value of  b is : = %f", a, b);
+
+return 0;
+}
